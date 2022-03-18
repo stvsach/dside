ee7a6145b70c3288e7c923334b27ed063d8cec46
Steven Sachio <stevensachio1506@gmail.com>
2022-03-18 17:01:24 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from setuptools import find_packages, setup
 
+__version__ = '0.1.1'
+short_description = 'Design space identification tool for plotting and analysing design spaces (2D and 3D).'
 with open("README.md", "r") as fh:
     long_description = fh.read()
     
 setup(
     name = 'dside',
-    version = '0.1.0',
+    version = __version__,
     author = 'Steven Sachio',
     author_email = 'stevensachio1506@gmail.com',
-    description = 'Design space identification tools using pandas dataframe and MATLAB engine',
+    description = short_description,
     long_description = long_description,
     license = 'MIT',
+    url = 'https://github.com/stvsach/dside',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Programming Language :: Python :: 3',
@@ -18,8 +21,11 @@ setup(
         'Operating System :: OS Independent',
         'Intended Audience :: Education',
     ],                                               # Information to filter the project on PyPi website
-    packages = find_packages(),                      # List of all python modules to be installed
+    packages = find_packages(where = 'src'),         # List of all python modules to be installed
+    package_dir = {'': 'src'},
     python_requires='>=3.9',                         # Minimum version requirement of the package
     py_modules=['dside'],                            # Name of the python package
-    install_requires=[]                              # Install other dependencies if any
+    install_requires=['numpy',
+                      'matplotlib',
+                      'pandas']                              # Install other dependencies if any
 )
