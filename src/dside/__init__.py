5d27923a5a83e445340db128a8c9ebac992a442a
Steven Sachio <stevensachio1506@gmail.com>
2022-05-15 16:26:19 +0100

    update __init__.py to increase speed

--- src/dside/__init__.py
+++ src/dside/__init__.py
@@ -1,6 +1,1 @@
 from dside.DSI import*
-
-try:
-    import matlab.engine
-except ModuleNotFoundError:
-    print('WARNING: matlab engine not found. Alpha shape of design space cannot be calculated.\nInstall from: https://uk.mathworks.com/help/matlab/matlab_external/install-the-matlab-engine-for-python.html')
