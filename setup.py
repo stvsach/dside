e53e0d4b5f4d91359bc201525746010dc5626a4a
Steven Sachio <stevensachio1506@gmail.com>
2022-07-28 10:41:42 +0100

    ver bump to v0.1.7.6

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-__version__ = '0.1.7.5'
+__version__ = '0.1.7.6'
 short_description = 'Design space identification tool for plotting and analysing design'+\
     ' spaces (2D and 3D).'
 with open("README.md", "r") as fh:
