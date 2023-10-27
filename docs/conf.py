34773220ec6c1fe08a53f5c823c41bcc6c0036da
Steven Sachio <stevensachio1506@gmail.com>
2023-10-27 19:13:34 +0100

    updated docs to v2.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'dside'
 copyright = '2022, Steven Sachio'
 author = 'Steven Sachio'
-release = '2.0.1'
+release = '2.2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
